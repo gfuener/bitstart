7321c0d0164ec2af411f6690d7d20d5de434a695
Greg Fuener <gregory.fuener@ge.com>
2013-07-10 17:02:05 +0000

    modified

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
+var buffer = require('buffer').Buffer;
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    buffer = fs.readFileSync('index.html');  
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
